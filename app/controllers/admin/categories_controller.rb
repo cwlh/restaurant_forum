059c4cd15178e8217d1cc91ba898d8800fa6345a
chiwei_hsu <giu201505@gmail.com>
2017-12-16 01:54:47 +0800

    add form on index page for creating new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,8 +4,24 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice]="成功新增餐廳種類"
+      redirect_to admin_categories_path
+    else
+      flash[:alert]="儲存失敗，請重新輸入"
+      @categories = Category.all
+      render :index
+    end
+  end
 
+  private
 
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
