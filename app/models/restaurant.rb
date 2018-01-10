a67a434a3c0b78ef94fd39f2103e3a4ded7ba8f0
chiwei_hsu <giu201505@gmail.com>
2018-01-11 00:18:15 +0800

    create favorite model and setup relation among user, favorite and restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,8 @@ class Restaurant < ApplicationRecord
   delegate :name, to: :category, prefix: true, allow_nil: true
 
   has_many :comments, dependent: :destroy   #以後刪除 Restaurant 時，就可以一併刪掉關聯的評論
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
+
 end
