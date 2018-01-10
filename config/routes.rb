82b9c50c5e92a4bf4331f25102c634e2d51cef8e
chiwei_hsu <giu201505@gmail.com>
2018-01-11 00:31:10 +0800

    setup routes for favorite / unfavorite

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
     member do
       get :dashboard
     end
+
+    member do
+      post :favorite
+      post :unfavorite
+    end
   end
   resources :users, only: [:show, :edit, :update]
   resources :categories, only: :show
