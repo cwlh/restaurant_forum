c31501ea0771b4a22e088c09623507dac06b494f
chiwei_hsu <giu201505@gmail.com>
2018-01-11 15:13:37 +0800

    setup relation among restaurant, user and like

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,10 +5,17 @@ class Restaurant < ApplicationRecord
   belongs_to :category, optional: true
   delegate :name, to: :category, prefix: true, allow_nil: true
 
-  has_many :comments, dependent: :destroy   #以後刪除 Restaurant 時，就可以一併刪掉關聯的評論
+  #以後刪除 Restaurant 時，就可以一併刪掉關聯的評論
+  has_many :comments, dependent: :destroy
 
+  # 「餐廳被很多使用者收藏」的多對多關聯
+  # 將關聯名稱自訂為 :favorited_users
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   def is_favorited?(user)
     self.favorited_users.include?(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,13 @@ class User < ApplicationRecord
   has_many :restaurants, through: :comments
 
   # 「使用者收藏很多餐廳」的多對多關聯
+  # 由於使用 restaurants 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :favorited_restaurants
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
 
   def admin?
     self.role == "admin"
