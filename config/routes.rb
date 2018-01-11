7af421ee53a85f12fb9dd19a68c17a303fdd9ad4
chiwei_hsu <giu201505@gmail.com>
2018-01-11 15:22:59 +0800

    setup routes for like / unlike

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,24 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
 
-    # 瀏覽所有餐廳的最新動態
     collection do
+      # 瀏覽所有餐廳的最新動態
+      # GET restaurants/feeds
       get :feeds
     end
 
-    # 瀏覽個別餐廳的 Dashboard
     member do
+      # 瀏覽個別餐廳的 Dashboard
+      # GET restaurants/:id/dashboard
       get :dashboard
-    end
 
-    member do
+      # 收藏 / 取消收藏
       post :favorite
       post :unfavorite
+
+      # 喜歡 / 取消喜歡
+      post :like
+      post :unlike
     end
   end
   resources :users, only: [:show, :edit, :update]
