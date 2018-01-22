43f60d07468621eb83d952759c7a4ecedbeb980d
chiwei_hsu <giu201505@gmail.com>
2018-01-23 00:59:15 +0800

    add set_restaurant method and refactoring RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite, :like, :unlike]
 
   def index
     @restaurants = Restaurant.order(:id).page(params[:page]).per(9)
@@ -6,7 +7,6 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
     @comment = Comment.new
   end
 
@@ -16,30 +16,25 @@ class RestaurantsController < ApplicationController
   end
 
   def dashboard
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def favorite
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.favorites.create!(user: current_user)
     redirect_back(fallback_location: root_path)
   end
 
   def unfavorite
-    @restaurant = Restaurant.find(params[:id])
     favorites = Favorite.where(restaurant: @restaurant, user: current_user)
     favorites.destroy_all
     redirect_back(fallback_location: root_path)
   end
 
   def like
-    @restaurant = Restaurant.find(params[:id])
     Like.create!(restaurant: @restaurant, user: current_user)
     redirect_back(fallback_location: root_path)
   end
 
   def unlike
-    @restaurant = Restaurant.find(params[:id])
     like = Like.where(restaurant: @restaurant, user: current_user)
     like.destroy_all
     redirect_back(fallback_location: root_path)
@@ -47,5 +42,11 @@ class RestaurantsController < ApplicationController
 
   def ranking
     @restaurants = Restaurant.order(favorites_count: :desc).limit(10)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
